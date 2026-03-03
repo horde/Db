f92cf208915bdcc3b978cf321efd0740f9ed0b48
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-03 07:38:32 +0100

    test(parser): split StatementParser test for PSR-0 and PSR-4
    Split StatementParserTest into two separate test classes:
    - Psr0StatementParserTest: Tests the legacy PSR-0 implementation (lib/)
    - Psr4StatementParserTest: Tests the modern PSR-4 implementation (src/)
    
    The original StatementParserTest.php now contains only a deprecation notice
    pointing to the new test files.
    
    Added additional test coverage for PSR-4 StatementParser:
    - testParserIteratesOverStatements: Verifies Iterator interface
    - testParserHandlesStringInput: Tests string path constructor
    
    Both test files work with PHPUnit 11.5 and 13.0.

---
+++ test/Psr0StatementParserTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright 2006-2017 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @category Horde
+ * @package  Db
+ * @license  http://www.horde.org/licenses/bsd
+ */
+
+namespace Horde\Db\Test;
+
+use Horde\Test\TestCase;
+use SplFileObject;
+use Horde_Db_StatementParser;
+
+/**
+ * Test for PSR-0 StatementParser (Horde_Db_StatementParser).
+ *
+ * This tests the legacy PSR-0 implementation in lib/ directory.
+ *
+ * @category Horde
+ * @package  Db
+ * @license  http://www.horde.org/licenses/bsd
+ * @covers   Horde_Db_StatementParser
+ */
+class Psr0StatementParserTest extends TestCase
+{
+    public function testParserFindsMultilineCreateStatement()
+    {
+        $expected = [
+            'DROP TABLE IF EXISTS `exp_actions`',
+            'SET @saved_cs_client     = @@character_set_client',
+            'SET character_set_client = utf8',
+            'CREATE TABLE `exp_actions` (
+              `action_id` int(4) unsigned NOT NULL auto_increment,
+              `class` varchar(50) NOT NULL,
+              `method` varchar(50) NOT NULL,
+              PRIMARY KEY  (`action_id`)
+            ) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1',
+            'SET character_set_client = @saved_cs_client',
+        ];
+        $this->assertParser($expected, 'drop_create_table.sql');
+    }
+
+    public function assertParser(array $expectedStatements, $filename)
+    {
+        $file = new SplFileObject(__DIR__ . '/fixtures/' . $filename, 'r');
+        $parser = new Horde_Db_StatementParser($file);
+
+        foreach ($expectedStatements as $i => $expected) {
+            // Strip any whitespace before comparing the strings.
+            $this->assertEquals(
+                preg_replace('/\s/', '', $expected),
+                preg_replace('/\s/', '', $parser->next()),
+                "Parser differs on statement #$i"
+            );
+        }
+    }
+}
---
+++ test/Psr4StatementParserTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Copyright 2006-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @category Horde
+ * @package  Db
+ * @license  http://www.horde.org/licenses/bsd
+ */
+
+declare(strict_types=1);
+
+namespace Horde\Db\Test;
+
+use Horde\Test\TestCase;
+use SplFileObject;
+use Horde\Db\StatementParser;
+
+/**
+ * Test for PSR-4 StatementParser (Horde\Db\StatementParser).
+ *
+ * This tests the modern PSR-4 implementation in src/ directory.
+ *
+ * @category Horde
+ * @package  Db
+ * @license  http://www.horde.org/licenses/bsd
+ * @covers   \Horde\Db\StatementParser
+ */
+class Psr4StatementParserTest extends TestCase
+{
+    public function testParserFindsMultilineCreateStatement(): void
+    {
+        $expected = [
+            'DROP TABLE IF EXISTS `exp_actions`',
+            'SET @saved_cs_client     = @@character_set_client',
+            'SET character_set_client = utf8',
+            'CREATE TABLE `exp_actions` (
+              `action_id` int(4) unsigned NOT NULL auto_increment,
+              `class` varchar(50) NOT NULL,
+              `method` varchar(50) NOT NULL,
+              PRIMARY KEY  (`action_id`)
+            ) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1',
+            'SET character_set_client = @saved_cs_client',
+        ];
+        $this->assertParser($expected, 'drop_create_table.sql');
+    }
+
+    public function testParserIteratesOverStatements(): void
+    {
+        $file = new SplFileObject(__DIR__ . '/fixtures/drop_create_table.sql', 'r');
+        $parser = new StatementParser($file);
+
+        $statements = [];
+        foreach ($parser as $statement) {
+            $statements[] = $statement;
+        }
+
+        $this->assertCount(5, $statements);
+        $this->assertStringContainsString('DROP TABLE', $statements[0]);
+        $this->assertStringContainsString('CREATE TABLE', $statements[3]);
+    }
+
+    public function testParserHandlesStringInput(): void
+    {
+        $parser = new StatementParser(__DIR__ . '/fixtures/drop_create_table.sql');
+
+        $count = 0;
+        foreach ($parser as $statement) {
+            $count++;
+        }
+
+        $this->assertEquals(5, $count);
+    }
+
+    protected function assertParser(array $expectedStatements, string $filename): void
+    {
+        $file = new SplFileObject(__DIR__ . '/fixtures/' . $filename, 'r');
+        $parser = new StatementParser($file);
+
+        foreach ($expectedStatements as $i => $expected) {
+            // Strip any whitespace before comparing the strings.
+            $this->assertEquals(
+                preg_replace('/\s/', '', $expected),
+                preg_replace('/\s/', '', $parser->next()),
+                "Parser differs on statement #$i"
+            );
+        }
+    }
+}
